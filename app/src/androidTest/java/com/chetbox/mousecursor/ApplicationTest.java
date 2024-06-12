9eed190603e7878c41f741df1d26f5f121720d4e
hikyuu <pixiuyouzai@qq.com>
2024-06-12 14:26:06 +0800

    准心和鼠标指针

--- app/src/androidTest/java/com/chetbox/mousecursor/ApplicationTest.java
+++ app/src/androidTest/java/com/chetbox/mousecursor/ApplicationTest.java
@@ -1,13 +1,13 @@
 package com.chetbox.mousecursor;
 
 import android.app.Application;
-import android.test.ApplicationTestCase;
+//import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super();
-    }
-}
+//public class ApplicationTest extends ApplicationTestCase<Application> {
+//    public ApplicationTest() {
+//        super();
+//    }
+//}
--- app/src/main/java/com/chetbox/mousecursor/MouseAccessibilityService.java
+++ app/src/main/java/com/chetbox/mousecursor/MouseAccessibilityService.java
@@ -4,19 +4,14 @@ import android.accessibilityservice.AccessibilityService;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
-import android.graphics.Rect;
 import android.net.LocalServerSocket;
 import android.net.LocalSocket;
-import android.net.LocalSocketAddress;
 import android.os.Build;
 import android.os.Handler;
-import android.os.SystemClock;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -25,83 +20,31 @@ import android.view.View;
 import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
 import android.view.accessibility.AccessibilityEvent;
-import android.view.accessibility.AccessibilityNodeInfo;
-import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketException;
-import java.sql.Time;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.TimeZone;
-import java.util.Timer;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MouseAccessibilityService extends AccessibilityService {
 
     private static final String TAG = MouseAccessibilityService.class.getName();
 
     private View cursorView;
+    private View accurateView;
     private LayoutParams cursorLayout;
+    private LayoutParams accurateLayout;
     private WindowManager windowManager;
     private NotificationManager notificationManager;
     private LocalServerSocket serverSocket;
-    private ReceiveThread receiveThread;
-
-    private static void logNodeHierachy(AccessibilityNodeInfo nodeInfo, int depth) {
-        Rect bounds = new Rect();
-        nodeInfo.getBoundsInScreen(bounds);
-
-        StringBuilder sb = new StringBuilder();
-        if (depth > 0) {
-            for (int i = 0; i < depth; i++) {
-                sb.append("  ");
-            }
-            sb.append("\u2514 ");
-        }
-        sb.append(nodeInfo.getClassName());
-        sb.append(" (").append(nodeInfo.getChildCount()).append(")");
-        sb.append(" ").append(bounds.toString());
-        if (nodeInfo.getText() != null) {
-            sb.append(" - \"").append(nodeInfo.getText()).append("\"");
-        }
-        Log.v(TAG, sb.toString());
-
-        for (int i = 0; i < nodeInfo.getChildCount(); i++) {
-            AccessibilityNodeInfo childNode = nodeInfo.getChild(i);
-            if (childNode != null) {
-                logNodeHierachy(childNode, depth + 1);
-            }
-        }
-    }
-
-    private static AccessibilityNodeInfo findSmallestNodeAtPoint(AccessibilityNodeInfo sourceNode, int x, int y) {
-        Rect bounds = new Rect();
-        sourceNode.getBoundsInScreen(bounds);
-
-        if (!bounds.contains(x, y)) {
-            return null;
-        }
-
-        for (int i = 0; i < sourceNode.getChildCount(); i++) {
-            AccessibilityNodeInfo nearestSmaller = findSmallestNodeAtPoint(sourceNode.getChild(i), x, y);
-            if (nearestSmaller != null) {
-                return nearestSmaller;
-            }
-        }
-        return sourceNode;
-    }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-
+        Log.d(TAG, "onAccessibilityEvent");
+        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+            Log.d(TAG, event.getPackageName().toString());
+        }
     }
 
     @Override
@@ -115,8 +58,41 @@ public class MouseAccessibilityService extends AccessibilityService {
         Log.d(TAG, "onCreate()");
         super.onCreate();
 
-        cursorView = View.inflate(getBaseContext(), R.layout.cursor, null);
+        initMouseCursor();
+        initAccurate();
 
+//        int LAYOUT_FLAG;
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+//        } else {
+//            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_PHONE;
+//        }
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        ReceiveThread receiveThread = new ReceiveThread();
+        receiveThread.start();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void initAccurate() {
+        accurateView = View.inflate(getBaseContext(), R.layout.accurate, null);
+        accurateView.setVisibility(View.INVISIBLE);
+        accurateLayout = new LayoutParams(
+                LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT,
+                LayoutParams.TYPE_APPLICATION_OVERLAY,
+                LayoutParams.FLAG_DISMISS_KEYGUARD |
+                        LayoutParams.FLAG_NOT_FOCUSABLE |
+                        LayoutParams.FLAG_NOT_TOUCHABLE |
+                        LayoutParams.FLAG_LAYOUT_IN_SCREEN |
+                        LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                PixelFormat.TRANSLUCENT
+        );
+        accurateLayout.gravity = Gravity.CENTER;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void initMouseCursor() {
+        cursorView = View.inflate(getBaseContext(), R.layout.cursor, null);
 
         cursorLayout = new LayoutParams(
                 LayoutParams.WRAP_CONTENT,
@@ -132,26 +108,20 @@ public class MouseAccessibilityService extends AccessibilityService {
         cursorLayout.gravity = Gravity.TOP | Gravity.START;
         cursorLayout.x = 0;
         cursorLayout.y = 0;
-        cursorLayout.height = 24;
-        cursorLayout.width = 24;
-
-//        int LAYOUT_FLAG;
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
-//        } else {
-//            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_PHONE;
-//        }
-        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-        receiveThread = new ReceiveThread();
-        receiveThread.start();
-
+//        cursorLayout.height = 24;
+//        cursorLayout.width = 24;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (windowManager != null && cursorView != null) {
+        if (windowManager != null) {
+            if (cursorView != null) {
             windowManager.removeView(cursorView);
+            }
+            if (accurateView != null) {
+                windowManager.removeView(accurateView);
+            }
         }
         if (serverSocket != null) {
             try {
@@ -160,18 +130,6 @@ public class MouseAccessibilityService extends AccessibilityService {
                 Log.e(TAG, "socket关闭异常");
             }
         }
-    }
-
-    private void click() {
-        Log.d(TAG, String.format("Click [%d, %d]", cursorLayout.x, cursorLayout.y));
-        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
-        if (nodeInfo == null) return;
-        AccessibilityNodeInfo nearestNodeToMouse = findSmallestNodeAtPoint(nodeInfo, cursorLayout.x, cursorLayout.y + 50);
-        if (nearestNodeToMouse != null) {
-            logNodeHierachy(nearestNodeToMouse, 0);
-            nearestNodeToMouse.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-        }
-        nodeInfo.recycle();
     }
 
     private class ServerThread implements Runnable {
@@ -185,6 +143,7 @@ public class MouseAccessibilityService extends AccessibilityService {
             br = new BufferedReader(new InputStreamReader(inputStream));
         }
 
+         AtomicInteger showMouse = new AtomicInteger(0);
         @Override
         public void run() {
             try {
@@ -196,15 +155,23 @@ public class MouseAccessibilityService extends AccessibilityService {
                         if (pos.length < 3) continue;
                         int posX = Integer.parseInt(pos[0]);
                         int posY = Integer.parseInt(pos[1]);
-                        int gameMap = Integer.parseInt(pos[2]);
+                        int whetherToShow = Integer.parseInt(pos[2]);
                         cursorLayout.x = posX;
                         cursorLayout.y = posY;
                         new Handler(getMainLooper()).post(() -> {
                             try {
-                                if (gameMap > 0) {
-                                    cursorView.setVisibility(View.INVISIBLE);
-                                } else {
-                                    cursorView.setVisibility(View.VISIBLE);
+
+                                if (showMouse.get() != whetherToShow) {
+//                                    Log.d(TAG, "showMouse:"+showMouse + "  whetherToShow:" + whetherToShow);
+                                    if (whetherToShow > 0) {
+                                        accurateView.setVisibility(View.VISIBLE);
+                                        cursorView.setVisibility(View.INVISIBLE);
+                                    } else {
+                                        accurateView.setVisibility(View.INVISIBLE);
+                                        cursorView.setVisibility(View.VISIBLE);
+                                    }
+                                    showMouse.set(whetherToShow);
+                                    windowManager.updateViewLayout(accurateView, accurateLayout);
                                 }
                                 windowManager.updateViewLayout(cursorView, cursorLayout);
                             } catch (NumberFormatException e) {
@@ -238,6 +205,10 @@ public class MouseAccessibilityService extends AccessibilityService {
         }
     }
 
+    @Override
+    public void onServiceConnected() {
+        Log.d(TAG, "onServiceConnected()");
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -246,6 +217,7 @@ public class MouseAccessibilityService extends AccessibilityService {
         Log.d(TAG, "onStartCommand()");
         if (cursorView.getWindowId() == null) {
             windowManager.addView(cursorView, cursorLayout);
+            windowManager.addView(accurateView, accurateLayout);
         }
         String NOTIFICATION_CHANNEL_ID = "com.chetbox.mousecursor";
         String channelName = "鼠标指针服务";
